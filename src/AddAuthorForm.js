adbfd83a184471c33cf2472a35b8d4e9d79e84a3
viatenaw <viatenaw@gmail.com>
2019-01-25 01:43:51 +0530

    initial Commit

---
+++ src/AddAuthorForm.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import './AddAuthorForm.css';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+class AuthorForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            name:'',
+            imageUrl:'',
+            books:[],
+            bookTemp:''
+        }
+       this.onFieldChange = this.onFieldChange.bind(this);  
+       this.handleSubmit = this.handleSubmit.bind(this);
+       this.handleAdd = this.handleAdd.bind(this);
+    }
+     onFieldChange =(event)=>{
+        this.setState({
+            [event.target.name]:event.target.value
+        });
+    }
+    handleSubmit =(event)=>{
+        event.preventDefault();
+        this.props.onAddAuthor(this.state);
+    }
+    handleAdd=(event)=>{
+        this.setState({
+            books:this.state.books.concat([this.state.bookTemp]),
+            bookTemp:''
+        });
+    }
+
+    render(){
+        return(
+        <form onSubmit={this.handleSubmit} >
+            <div className="AddAuthorForm_input">
+                 <label htmlFor="name" >Name</label>
+                 <input type="text" name="name" value={this.state.name}  onChange={this.onFieldChange} />
+            </div>
+
+            <div className="AddAuthorForm_input">
+              <label htmlFor="imageUrl" >Image Url</label>
+              <input type="text" name="imageUrl" imageUrl={this.state.imageUrl} onChange={this.onFieldChange} />
+           </div>
+           
+
+            <div className="AddAuthorForm_input" >
+               <label htmlFor="bookTemp" >Books</label>
+                {this.state.books.map(book=> <p key={book}>{book}</p>)}
+                <input type="text" name="bookTemp" value={this.state.bookTemp} onChange={this.onFieldChange} />
+                <input type="button" value="+" onClick={this.handleAdd} />
+             </div>
+
+             <input type="submit" value="Add" />
+        </form>
+    );
+    }
+}
+
+const AddAuthorForm=({match, onAddAuthor})=>{
+    return(
+        <div className="AddAuthorForm">   
+            <h1>Add Author</h1>
+            <AuthorForm onAddAuthor={onAddAuthor}/>
+         </div>
+    );
+}
+
+const mapDispatchToProps=(dispatch, props)=>{
+    return{
+        onAddAuthor:(author)=>{
+            dispatch({type:'ADD_AUTHOR', author});
+            props.history.push('/');
+        }
+    };
+}
+
+export default withRouter(connect(()=>{}, mapDispatchToProps )(AddAuthorForm));
